40c505511c332cacf74206970533ddd8451f9a30
Chandra Bhan Singh Chouhan <chandra_bhan@readybytes.in>
2020-05-23 17:53:33 +0530

    commit message here

--- src/Console/Commands/EnvCheckCommand.php
+++ src/Console/Commands/EnvCheckCommand.php
@@ -5,7 +5,7 @@ namespace Chandra9999\EnvChecker\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
-use Jsefton\DotEnv\Parser;
+use JSefton\DotEnv\Parser;
 
 class EnvCheckCommand extends Command
 {   
